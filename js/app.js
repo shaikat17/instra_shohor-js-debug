fe3969f68bfa2cd5bc717965446fee263342406b
shaikat17 <shaikatpal56@gmail.com>
2023-03-21 23:18:53 +0600

    post username and userImage problem is solved

--- js/app.js
+++ js/app.js
@@ -51,6 +51,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
+  // console.log(post)
     const image = post.image;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
@@ -62,9 +63,9 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
-                  <a href="#" class="post__user">phero</a>
+                  <a href="#" class="post__user">${post.userName}</a>
                 </div>
 
                 <button class="post__more-options">
